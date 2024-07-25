e2b8ea3de0c57f3719e1c98f99e3d8e3a45ff22b
kshitij <patelkshitij20@gmail.com>
2024-07-25 13:49:44 -0300

    Item updated.

--- apps/application/models/ItemModel.py
+++ apps/application/models/ItemModel.py
@@ -1,15 +1,27 @@
 from django.db import models
 from apps.accounts.models import UserModel
 from apps.application.models.CategoryModel import CategoryModel
+from django.utils.translation import gettext_lazy as _
 
 
 class ItemModel(models.Model):
+    class Condition(models.TextChoices):
+        NEW = 'NW', _('New')
+        HARDLY_USED = 'HU', _('Hardly Used')
+        USED = 'US', _('Used')
+        ROUGHLY_USED = 'RS', _('Roughly Used')
+        WORSE = 'WR', _('Worse')
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
     seller = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     price = models.FloatField(null=False, blank=False)
     category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, null=True, blank=True)
+    condition = models.CharField(choices=Condition.choices, max_length=2, null=False, blank=False,
+                                 default=Condition.USED)
+    is_sold = models.BooleanField(default=False)
+    is_bidding_enabled = models.BooleanField(default=False)
     createdAt = models.DateTimeField(auto_now_add=True)
 
     class Meta:
