68aac2e749f7fb8c236c6bd2357953484d8f3315
kshitij <patelkshitij20@gmail.com>
2024-07-30 19:47:02 -0300

    Update orders and list orders APIS

--- apps/application/serializers/AddressSerializer.py
+++ apps/application/serializers/AddressSerializer.py
@@ -13,6 +13,15 @@ class AddressSerializer(serializers.ModelSerializer):
         address = AddressModel.objects.create(**validated_data)
         return address
 
+    def update(self, instance, validated_data):
+        instance.apartment = validated_data.get('apartment', instance.apartment)
+        instance.address_line = validated_data.get('address_line', instance.address_line)
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+        instance.postal_code = validated_data.get('postal_code', instance.postal_code)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+
     def to_representation(self, instance):
         return {
             'id': instance.id,
--- apps/application/views/OrderViews.py
+++ apps/application/views/OrderViews.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from ..models import OrderModel, OrderItemModel, ShippingAddressModel
+from ..models import OrderModel, OrderItemModel, ShippingAddressModel, AddressModel
 from ..serializers import OrderSerializer, ShippingAddressSerializer, AddressSerializer, \
     ContactDetailsSerializer, OrderDetailsSerializer
 
@@ -29,7 +29,11 @@ def add_shipping_address(request):
     shipping_address_serializer.is_valid(raise_exception=True)
     shipping_address = shipping_address_serializer.save()
 
-    return Response(shipping_address, status=status.HTTP_201_CREATED)
+    shipping_addresses = ShippingAddressModel.objects.filter(user=user_id)
+
+    serializer = ShippingAddressSerializer(shipping_addresses, many=True)
+
+    return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
@@ -44,7 +48,30 @@ def get_shipping_addresses(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_shipping_address(request, shipping_address_id):
+    user = request.user
+    user_id = user.id
+
+    shipping_address = request.data
+    shipping_address_object = ShippingAddressModel.objects.get(id=shipping_address_id)
+    address = shipping_address_object.address
+    address.apartment = shipping_address.get('apartment')
+    address.address_line = shipping_address.get('address_line')
+    address.city = shipping_address.get('city')
+    address.state = shipping_address.get('state')
+    address.postal_code = shipping_address.get('postal_code')
+    address.country = shipping_address.get('country')
+    address.save()
+
+    shipping_addresses = ShippingAddressModel.objects.filter(user=user_id)
+    serializer = ShippingAddressSerializer(shipping_addresses, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def add_order(request):
     user = request.user
@@ -103,7 +130,7 @@ def get_orders(request):
     user = request.user
     user_id = user.id
     orders = OrderModel.objects.filter(user_id=user_id)
-    serializer = OrderSerializer(orders, many=True)
+    serializer = OrderDetailsSerializer(orders, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -115,13 +142,48 @@ def get_order_details(request, order_id):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def update_order_details(request, order_id):
     user = request.user
     user_id = user.id
     order = OrderModel.objects.get(id=order_id)
 
-    serializer = OrderDetailsSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
+    billing_address = request.data.get('billing_address', None)
+    shipping_address = request.data.get('shipping_address', None)
+    contact_details = request.data.get('contact_details', None)
+    items = request.data.get('items', None)
 
+    if shipping_address:
+        shipping_address = ShippingAddressModel.objects.get(id=shipping_address)
+        order.shipping_address = shipping_address
+
+    if billing_address:
+        billing_address_object = order.billing_address
+        billing_address_object.apartment = billing_address.get('apartment')
+        billing_address_object.address_line = billing_address.get('address_line')
+        billing_address_object.city = billing_address.get('city')
+        billing_address_object.state = billing_address.get('state')
+        billing_address_object.postal_code = billing_address.get('postal_code')
+        billing_address_object.country = billing_address.get('country')
+        billing_address_object.save()
+
+    if contact_details:
+        contact_details_object = order.contact_details
+        contact_details_object.first_name = contact_details.get('first_name')
+        contact_details_object.last_name = contact_details.get('last_name')
+        contact_details_object.email = contact_details.get('email')
+        contact_details_object.phone = contact_details.get('phone')
+        contact_details_object.save()
+
+    order.save()
+
+    if items:
+        OrderItemModel.objects.filter(order_id=order_id).delete()
+
+        for item in items:
+            OrderItemModel.objects.create(order_id=order_id, item_id=item)
+
+    order_object = OrderModel.objects.get(id=order_id)
+
+    return Response(OrderDetailsSerializer(order_object).data, status=status.HTTP_200_OK)
